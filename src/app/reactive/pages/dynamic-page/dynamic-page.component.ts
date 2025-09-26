1b99b196532a80638f30f58ff1820b5326faeb2f
Tu Nombre <a22300241@ceti.mx>
2025-09-26 14:56:19 -0600

    feat: integrate FormUtils for enhanced form validation and error handling in dynamic page

--- src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
+++ src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
@@ -1,6 +1,7 @@
 import { JsonPipe } from '@angular/common';
 import { Component, inject } from '@angular/core';
 import { FormArray, FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormUtils } from '../../../utils/form-utils';
 
 @Component({
   selector: 'app-dynamic-page',
@@ -8,6 +9,7 @@ import { FormArray, FormBuilder, FormGroup, ReactiveFormsModule, Validators } fr
   templateUrl: './dynamic-page.component.html',
 })
 export class DynamicPageComponent {
+  formUtils=FormUtils;
   private fb=inject(FormBuilder);
   myForm:FormGroup=this.fb.group({
     name:['',[Validators.required,Validators.minLength(3)]],
--- src/app/utils/form-utils.ts
+++ src/app/utils/form-utils.ts
@@ -1,12 +1,8 @@
-import { FormGroup } from "@angular/forms";
+import { FormArray, FormGroup, ValidationErrors } from "@angular/forms";
 
 export class FormUtils {
-  static isValidField(form:FormGroup, fildName:string):boolean|null{
-    return (!!form.controls[fildName].errors && form.controls[fildName].touched)
-  }
-  static getFieldError(form:FormGroup,fildName:string):string|null{
-    if(!form.controls[fildName]) return null;
-    const errors=form.controls[fildName].errors || {};
+  static getTextErrors(errors:ValidationErrors)
+  {
     for(const key of Object.keys(errors)){
       switch(key){
         case 'required':
@@ -18,5 +14,23 @@ export class FormUtils {
       }
     }
     return null;
+  }
+  static isValidField(form:FormGroup, fildName:string):boolean|null{
+    return (!!form.controls[fildName].errors && form.controls[fildName].touched)
+  }
+  static getFieldError(form:FormGroup,fildName:string):string|null{
+    if(!form.controls[fildName]) return null;
+    const errors=form.controls[fildName].errors || {};
+    return this.getTextErrors(errors);
+  }
+
+
+   static isValidFieldInArray(formArray:FormArray,index:number){
+    return formArray.controls[index].errors && formArray.controls[index].touched;
+  }
+  static getFieldErrorInArray(formArray:FormArray,index:number):string|null{
+    if(formArray.controls.length==0) return null;
+    const errors=formArray.controls[index].errors ?? {};
+    return this.getTextErrors(errors);
   }
 }
