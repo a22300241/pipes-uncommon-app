ec4c29da19fde2120342a1f96848735d8a982522
Tu Nombre <a22300241@ceti.mx>
2025-09-25 22:12:15 -0600

    feat: enhance form validation with dynamic error messages and field validation checks

--- src/app/reactive/pages/basic-page/basic-page.component.ts
+++ src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -1,3 +1,4 @@
+import { isValid } from './../../../../../node_modules/zod/src/v3/helpers/parseUtil';
 import { JsonPipe } from '@angular/common';
 import { Component, inject } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
@@ -9,7 +10,7 @@ import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } 
 })
 export class BasicPageComponent {
   private fb=inject(FormBuilder)
-  myForm=this.fb.group({
+  myForm:FormGroup=this.fb.group({
     name: ['',[Validators.required, Validators.minLength(3)],],
     price: [0, [Validators.required, Validators.min(10)]],
     inStorage: [0, [Validators.required, Validators.min(0)]],
@@ -19,4 +20,22 @@ export class BasicPageComponent {
   //  price: new FormControl(0),
   //  inStorage: new FormControl(0),
   //})
+  isValidField(fildName:string):boolean|null{
+    return !!this.myForm.controls[fildName].errors
+  }
+  getFieldError(fildName:string):string|null{
+    if(!this.myForm.controls[fildName]) return null;
+    const errors=this.myForm.controls[fildName].errors || {};
+    for(const key of Object.keys(errors)){
+      switch(key){
+        case 'required':
+          return 'Este campo es obligatorio';
+        case 'minlength':
+          return `Mínimo ${errors['minlength'].requiredLength} caracteres.`;
+        case 'min':
+          return `El valor mínimo es ${errors['min'].min}.`;
+      }
+    }
+    return null;
+  }
  }
