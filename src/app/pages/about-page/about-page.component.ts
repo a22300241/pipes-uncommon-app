e0748cbcca0ae81d0efa4ea2eeec54cc04e1d3c9
Tu Nombre <a22300241@ceti.mx>
2025-09-27 21:00:27 -0600

    feat: implement title component and enhance home page functionality

--- src/app/pages/about-page/about-page.component.ts
+++ src/app/pages/about-page/about-page.component.ts
@@ -1,8 +1,13 @@
-import { Component } from '@angular/core';
-
+import { afterEveryRender, afterNextRender, Component, effect } from '@angular/core';
+const log =(...messages:string[])=>
+{
+  console.log(`${messages[0]}%c${messages.slice(1).join(',')}`,'color: #bada55');
+};
 @Component({
   selector: 'app-about-page',
   imports: [],
   templateUrl: './about-page.component.html',
 })
-export class AboutPageComponent { }
+export class AboutPageComponent {
+
+}
