c5673c41c094668fc680fde0fd16a4a93b712a28
Tu Nombre <a22300241@ceti.mx>
2025-09-28 18:32:52 -0600

    feat: Add fullscreen map and navigation components
    - Implemented FullscreenMapPageComponent with basic template.
    - Created MarkersPageComponent and HausesPageComponent with respective templates.
    - Added NavbarComponent for navigation with dynamic route handling.
    - Updated app routing to include new pages and redirect to fullscreen by default.
    - Integrated Tailwind CSS for styling.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { FullscreenMapPageComponent } from './pages/fullscreen-map-page/fullscreen-map-page.component';
+import { MarkersPageComponent } from './pages/fullscreen-map-page/markers-page/markers-page.component';
+import { HausesPageComponent } from './pages/fullscreen-map-page/hauses-page/hauses-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path:'fullscreen',component:FullscreenMapPageComponent, title:'FullScreen Map' },
+  {path:'markers', component:MarkersPageComponent,title:'Marcadores'},
+  {path:'hauses', component:HausesPageComponent,title:'Propiedades disponibles'},
+  {path:'**', redirectTo:'fullscreen'}
+];
