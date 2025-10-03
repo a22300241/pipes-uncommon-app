caa0f62f7c1a42d91659140d9462f55523c52721
Tu Nombre <a22300241@ceti.mx>
2025-10-03 17:14:38 -0600

    cambios de la seccion 19 como paginar

--- src/app/products/services/products.service.ts
+++ src/app/products/services/products.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { Product, ProductsResponse } from '@products/interfaces/product.interface';
-import { firstValueFrom, Observable, tap } from 'rxjs';
+import { count, delay, firstValueFrom, Observable, of, pipe, tap } from 'rxjs';
 import { environment } from 'src/environments/environment';
 const baseUrl=environment.baseURL
 interface Options{
@@ -13,29 +13,28 @@ interface Options{
 @Injectable({providedIn: 'root'})
 export class ProductsService {
   private http=inject(HttpClient);
-
-  getProducts(opcions: Options): Promise<ProductsResponse> {
+  private productsCache=new Map<string,ProductsResponse>();
+  private productCache=new Map<string,Product>();
+  getProducts(opcions: Options): Observable<ProductsResponse> {
   const { limit=9, offset=0, gender='' } = opcions;
-  return firstValueFrom(
-    this.http.get<ProductsResponse>(`${baseUrl}/products`, {
+  const key=`${limit}-${offset}-${gender}`;
+  if(this.productsCache.has(key)){
+    return of(this.productsCache.get(key)!)
+  }
+  return this.http.get<ProductsResponse>(`${baseUrl}/products`, {
       params: { limit, offset, gender }
     }).pipe(
-    tap((resp) => console.log(resp)))
-  );
+    tap((resp) => console.log(resp)),
+    tap((resp)=> this.productsCache.set(key,resp))
+  )
+
 }
 getProductByIdSlug(idSlug:string):Observable<Product>{
-  return this.http.get<Product>(`${baseUrl}/products/${idSlug}`);
-
+  if(this.productCache.has(idSlug)){
+    return of(this.productCache.get(idSlug)!)
+  }
+  return this.http.get<Product>(`${baseUrl}/products/${idSlug}`).
+  pipe(
+    tap((product)=>this.productCache.set(idSlug,product)))
 }
-
-
- /* getProducts(opcions: Options): Observable<ProductsResponse> {
-  const { limit=9, offset=0, gender='' } = opcions;
-  return this.http.get<ProductsResponse>(`${baseUrl}/products`, {
-    params: { limit, offset, gender }
-  }).pipe(
-    tap((resp) => console.log(resp))
-  );
-}
- */
 }
